2b97c0e0fe69a226909ed16852757d1b90b82411
vishal patidar <44113789+patidar622@users.noreply.github.com>
2020-07-02 22:41:40 +0530

    Update Largest Number formed from an Array.cpp

--- Largest Number formed from an Array.cpp
+++ Largest Number formed from an Array.cpp
@@ -1,3 +1,38 @@
+/*
+/*
+This “comparator” function returns a value; convertible to bool,
+which basically tells us whether the passed “first” argument should be 
+placed before the passed “second” argument or not.
+*/
+// ye wala interview bit ka he
+
+bool comparator(string s1,string s2)
+{
+    if(s1+s2>=s2+s1)
+    return 1;
+    return 0;
+}
+string Solution::largestNumber(const vector<int> &A) {
+    vector<string>vis;
+    for(int i=0;i<A.size();i++)
+    {
+        vis.push_back(to_string(A[i]));
+    }
+    sort(vis.begin(),vis.end(),comparator);
+    string ans="";
+    for(int i=0;i<vis.size();i++)
+    {
+        ans+=vis[i];
+    }
+    while(ans[0]=='0' && ans.size()>1)
+    {
+        ans.erase(ans.begin()+0);
+    }
+    return ans;
+}
+*/
+// geeks ka he
+	
 #include<bits/stdc++.h>
 using namespace std;
 int compare(int i1,int i2)
